bb59b0199ff1ab83925f2500e750c693c3b1f45b
jameson.gelarman <jameson.gelarman@cvsu.edu.ph>
2025-02-08 16:47:08 +0000

    Initial commit

---
+++ components/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return(
+        <div className="bg-gray-600 min-h-screen flex flex-col justify-center items-center w-full">
+            
+        </div>
+    )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+export default function Navbar() {
+    const navItems = [
+        {
+            name: "Home",
+            url: "/",
+        },
+        {
+            name: "Products",
+            url: "#products",
+        },
+        {
+            name: "Our Store",
+            url: "#store",
+        },
+        {
+            name: "About",
+            url: "#about",
+        },
+        {
+            name: "Contact",
+            url: "#contact",
+        },
+    ]
+    return (
+        <div className="fixed top-0 w-full p-5 text-gray-200">
+            <div className="flex justify-around items-center w-full">
+                <h1 className="text-3xl">KoFi Roasters</h1>
+                <ul className="flex items-center gap-14">
+                    {navItems.map((item) => (
+                        <li key={item.name}>
+                            <a href={item.url} className="text-sm uppercase font-semibold">{item.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
